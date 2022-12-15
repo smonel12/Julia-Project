4d6e26feb7958bc1ec8f56619245312867c6ddc4
Shawn Monel <smonel@mit.edu>
2022-12-14 19:51:27 -0500

    Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
 using MyJuliaPackage
 using Documenter
 
-DocMeta.setdocmeta!(MyJuliaPackage, :DocTestSetup, :(using MyJuliaPackage); recursive=true)
+DocMeta.setdocmeta!(Julia-Project, :DocTestSetup, :(using MyJuliaPackage); recursive=true)
 
 makedocs(;
     modules=[MyJuliaPackage],
     authors="Shawn Monel <smonel@mit.edu> and contributors",
-    repo="https://github.com/smonel/MyJuliaPackage.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/smonel/Julia-Project.jl/blob/{commit}{path}#{line}",
     sitename="MyJuliaPackage.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://smonel.github.io/MyJuliaPackage.jl",
+        canonical="https://smonel.github.io/Julia-Project.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/smonel/MyJuliaPackage.jl",
+    repo="github.com/smonel/Julia-Project.jl",
     devbranch="main",
 )
